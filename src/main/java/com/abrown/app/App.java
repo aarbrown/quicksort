3cdc1ea63b5b5d23ad099433b859fc052015cbf3
Aaron Brown <abrown@pltw.org>
2017-01-15 12:28:31 -0500

    adds output from main app

--- src/main/java/com/abrown/app/App.java
+++ src/main/java/com/abrown/app/App.java
@@ -6,7 +6,12 @@ public class App
 {
     public static void main( String[] args )
     {
+        // pre-sorted
         int[] arr = { 55, 23, 26, 2, 18, 78, 23, 8, 2, 3 };
-        System.out.println( Arrays.toString(arr) );
+        System.out.println("Pre-sorted: " + Arrays.toString(arr) );
+
+        // post-sorted
+        Quicksort.sort(arr);
+        System.out.println("Post-sorted: " + Arrays.toString(arr) );
     }
 }
